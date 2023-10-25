ba187cd198503d1be6f9895f44d50ce94e438687
Nick Glynn <n.s.glynn@gmail.com>
2023-10-25 16:08:39 +1100

    Add flaaaavor to partials.go

--- partials.go
+++ partials.go
@@ -11,6 +11,8 @@ const (
 	partialTemplateFolder = "partials"
 )
 
+// partialEncoder will look for a specific {foo}.tpl.html
+// and then execute it by name so only useful for 1:1 mappoings
 func partialEncoder(wr io.Writer, partialName string, data any) error {
 
 	// Good to understand how the variables work
@@ -22,7 +24,7 @@ func partialEncoder(wr io.Writer, partialName string, data any) error {
 		logger.Error("Failed to parse template", "full_path", fmt.Sprintf("%s/%s", partialTemplateFolder, tmplFile), "error", err)
 		return err
 	}
-	err = tmpl.Execute(wr, data)
+	err = tmpl.Lookup(partialName).Execute(wr, data)
 	if err != nil {
 		logger.Error("Failed to execute template", "template", tmpl.Name, "error", err)
 		return err
